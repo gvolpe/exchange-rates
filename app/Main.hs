cf662a16e9718dc8e278b72ccf8a41d1228f9001
Gabriel Volpe <volpegabriel@gmail.com>
2019-06-09 15:48:09 -0600

    Implementing a rate limiter for API calls

--- app/Main.hs
+++ app/Main.hs
@@ -1,12 +1,20 @@
 module Main where
 
 import           Config
+import           Control.Concurrent             ( threadDelay )
+import           Data.Monoid                    ( (<>) )
 import           Forex                          ( showApiUsage
                                                 , showForex
                                                 )
+import           RateLimiter                    ( rateLimiter )
+
+wait :: IO ()
+wait = threadDelay (1000000 * 5)
 
 main :: IO ()
 main = do
   c <- loadConfig
   print c
-  showApiUsage (forex c)
+  rateLimiter 2 $ map
+    (\n -> putStrLn ("Triggered #" <> show n) >> wait >> showApiUsage (forex c))
+    [1 .. 10]
---
+++ src/RateLimiter.hs
@@ -0,0 +1,14 @@
+module RateLimiter where
+
+import           Control.Concurrent             ( threadDelay )
+import           Control.Monad.IO.Class         ( liftIO )
+import           Data.Functor                   ( void )
+import           Transient.Base
+import           Transient.Indeterminism
+
+rateLimiter :: Int -> [IO ()] -> IO ()
+rateLimiter n [] = pure ()
+rateLimiter n xs = void . keep' $ do
+  fa <- threads n $ choose xs
+  liftIO fa
+
